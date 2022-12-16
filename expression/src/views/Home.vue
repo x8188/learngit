87bc945592c428a95d99c02920a1277abf3806bc
x8188 <1341002157@qq.com>
2022-12-16 18:46:44 +0800

    tutorial

--- expression/src/views/Home.vue
+++ expression/src/views/Home.vue
@@ -4,13 +4,18 @@
 -->
 <template>
   <div class="home">
+    <el-carousel :interval="4000" height="400px">
+        <el-carousel-item v-for="item in 6" :key="item">
+          <h3 class="medium">{{ item }}</h3>
+        </el-carousel-item>
+    </el-carousel>
     <div id="components-layout-demo-basic">
       <div class="top-text" style="margin-top: 20px">
         <a-row type="flex" justify="center">
           <a-col :span="20">
             <el-card class="homecard">
               <div>
-                <p >
+                <p>
                   &nbsp; &nbsp;&nbsp;&nbsp; MaizeDeepCBA is a web server that
                   integrates maize chromatin interaction and deep learning
                   models for maize Gene expression prediction. Based on the
@@ -51,11 +56,13 @@
       <div style="margin-top: 20px">
         <el-row :gutter="40" type="flex">
           <el-col :span="12">
-            <el-card class="homecard" style="background-color: #296D7F;color:#FFFFFF">
-              <p style="font-weight: bold;font-size: 30px;">Introduction</p>
+            <el-card
+              class="homecard"
+              style="background-color: #296d7f; color: #ffffff"
+            >
+              <p style="font-weight: bold; font-size: 30px">Introduction</p>
               <p>
-                xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-                xxxxx
+                xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx xxxxx
                 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
                 xxxxxxxxxxx
                 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
@@ -67,7 +74,7 @@
             </el-card>
           </el-col>
           <el-col :span="12">
-            <el-card class="homecard">
+            <el-card class="homecard" style="background-color:#D7D4B6; color: #262626">
               <p>
                 &nbsp; &nbsp;&nbsp;&nbsp; xxxxxxxxxxxxxxxxxxx
                 xxxxxxxxxxxxxxxxxxx
@@ -145,15 +152,17 @@ export default {
 <style scoped>
 #components-layout-demo-basic .ant-layout-content {
   background: rgb(255, 255, 255);
-  color: black;
-}
+  color: black;  
 
+}
+#components-layout-demo-basic {
+  width: 80%;
+  margin: 0 auto;
+}
 .home {
   /* text-align: center; */
   /* height: 1000px; */
   /* background-color: rgb(0, 135, 172); */
-  width: 80%;
-  margin: 0 auto;
 }
 
 /* p::first-letter {
@@ -179,13 +188,32 @@ export default {
   border-color: #71d8ce !important;
   color: #fff !important;
 }
-.homecard{
+.homecard {
   text-align: center;
   height: 100%;
   /* background-color: #296D7F; */
 }
-.homecard p{
-  font-size: 20px; 
-  text-align: justify
+.homecard p {
+  font-size: 20px;
+  text-align: justify;
+}
+.el-carousel__item{
+  position:absolute ;
+  width: 100%;
+}
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #C0C0C0;
 }
 </style>
--- expression/src/views/Tutorial.vue
+++ expression/src/views/Tutorial.vue
@@ -1,13 +1,161 @@
 <template>
-  <div>tutorial</div>
+  <div>
+    <a-affix :offset-top="100" id="content">
+      <!-- <i class="el-icon-notebook-2" style="font-weight: bold; font-size: 40px"
+        >CONTENT</i
+      > -->
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+      >
+        <el-submenu index="1" @click="goTop(1)">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>导航一</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="1-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="1-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="1-4-1">选项1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="2" @click="goTop(2)">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3" @click="goTop(3)">
+          <i class="el-icon-document"></i>
+          <span slot="title">导航三</span>
+        </el-menu-item>
+        <el-menu-item index="4" @click="goTop(4)">
+          <i class="el-icon-setting"></i>
+          <span slot="title">导航四</span>
+        </el-menu-item>
+      </el-menu>
+    </a-affix>
+
+    <el-row class="tac" type="flex" justify="center">
+      <el-col :span="6" id="doc">
+        <i class="el-icon-s-management" ref="top1">导航1</i>
+        <h2>选项1</h2>
+        <p>
+          xxx xxxxxx xxxxxxx xxxxxxx xxxx xxxx xxxx xx xxx xxxx xxxxx xxx xx
+          xxxxx xxx xxx xxxx xxx xxxx xxx xxxx xxxxx xxx xxx xxxxx xx xxx xxx
+          xxxx xxx xx xx xxx xx xx x xxx xx xxx xxx xxx xx xxx xxx xxx xxxx xxx
+          xxx xxx xx xx xxx xxx xx xxx xxxx xx xxx xxx xx xxx xxx x xxxx x xx
+          xxx xx xx xxxx xxxx xxxx xxxxx xxxxxx xxxxx xxxxxx xxxx xxx xxx xx xxx
+          xx xxx xxx xxx xxx xxx xxx xxx xxxx xxx xx xxxx xxxx
+        </p>
+        <h2>选项2</h2>
+        <p>
+          xxx xxxxxx xxxxxxx xxxxxxx xxxx xxxx xxxx xx xxx xxxx xxxxx xxx xx
+          xxxxx xxx xxx xxxx xxx xxxx xxx xxxx xxxxx xxx xxx xxxxx xx xxx xxx
+          xxxx xxx xx xx xxx xx xx x xxx xx xxx xxx xxx xx xxx xxx xxx xxxx xxx
+          xxx xxx xx xx xxx xxx xx xxx xxxx xx xxx xxx xx xxx xxx x xxxx x xx
+          xxx xx xx xxxx xxxx xxxx xxxxx xxxxxx xxxxx xxxxxx xxxx xxx xxx xx xxx
+          xx xxx xxx xxx xxx xxx xxx xxx xxxx xxx xx xxxx xxxx
+        </p>
+        <h2>选项3</h2>
+        <p>
+          xxx xxxxxx xxxxxxx xxxxxxx xxxx xxxx xxxx xx xxx xxxx xxxxx xxx xx
+          xxxxx xxx xxx xxxx xxx xxxx xxx xxxx xxxxx xxx xxx xxxxx xx xxx xxx
+          xxxx xxx xx xx xxx xx xx x xxx xx xxx xxx xxx xx xxx xxx xxx xxxx xxx
+          xxx xxx xx xx xxx xxx xx xxx xxxx xx xxx xxx xx xxx xxx x xxxx x xx
+          xxx xx xx xxxx xxxx xxxx xxxxx xxxxxx xxxxx xxxxxx xxxx xxx xxx xx xxx
+          xx xxx xxx xxx xxx xxx xxx xxx xxxx xxx xx xxxx xxxx
+        </p>
+        <h2>选项4</h2>
+        <p>
+          xxx xxxxxx xxxxxxx xxxxxxx xxxx xxxx xxxx xx xxx xxxx xxxxx xxx xx
+          xxxxx xxx xxx xxxx xxx xxxx xxx xxxx xxxxx xxx xxx xxxxx xx xxx xxx
+          xxxx xxx xx xx xxx xx xx x xxx xx xxx xxx xxx xx xxx xxx xxx xxxx xxx
+          xxx xxx xx xx xxx xxx xx xxx xxxx xx xxx xxx xx xxx xxx x xxxx x xx
+          xxx xx xx xxxx xxxx xxxx xxxxx xxxxxx xxxxx xxxxxx xxxx xxx xxx xx xxx
+          xx xxx xxx xxx xxx xxx xxx xxx xxxx xxx xx xxxx xxxx
+        </p>
+
+        <i class="el-icon-s-management" ref="top2">导航2</i>
+        <p>
+          xxx xxxxxx xxxxxxx xxxxxxx xxxx xxxx xxxx xx xxx xxxx xxxxx xxx xx
+          xxxxx xxx xxx xxxx xxx xxxx xxx xxxx xxxxx xxx xxx xxxxx xx xxx xxx
+          xxxx xxx xx xx xxx xx xx x xxx xx xxx xxx xxx xx xxx xxx xxx xxxx xxx
+          xxx xxx xx xx xxx xxx xx xxx xxxx xx xxx xxx xx xxx xxx x xxxx x xx
+          xxx xx xx xxxx xxxx xxxx xxxxx xxxxxx xxxxx xxxxxx xxxx xxx xxx xx xxx
+          xx xxx xxx xxx xxx xxx xxx xxx xxxx xxx xx xxxx xxxx
+        </p>
+        <i class="el-icon-s-management" ref="top3">导航3</i>
+        <p>
+          xxx xxxxxx xxxxxxx xxxxxxx xxxx xxxx xxxx xx xxx xxxx xxxxx xxx xx
+          xxxxx xxx xxx xxxx xxx xxxx xxx xxxx xxxxx xxx xxx xxxxx xx xxx xxx
+          xxxx xxx xx xx xxx xx xx x xxx xx xxx xxx xxx xx xxx xxx xxx xxxx xxx
+          xxx xxx xx xx xxx xxx xx xxx xxxx xx xxx xxx xx xxx xxx x xxxx x xx
+          xxx xx xx xxxx xxxx xxxx xxxxx xxxxxx xxxxx xxxxxx xxxx xxx xxx xx xxx
+          xx xxx xxx xxx xxx xxx xxx xxx xxxx xxx xx xxxx xxxx
+        </p>
+        <i class="el-icon-s-management" ref="top4">导航4</i>
+        <p>
+          xxx xxxxxx xxxxxxx xxxxxxx xxxx xxxx xxxx xx xxx xxxx xxxxx xxx xx
+          xxxxx xxx xxx xxxx xxx xxxx xxx xxxx xxxxx xxx xxx xxxxx xx xxx xxx
+          xxxx xxx xx xx xxx xx xx x xxx xx xxx xxx xxx xx xxx xxx xxx xxxx xxx
+          xxx xxx xx xx xxx xxx xx xxx xxxx xx xxx xxx xx xxx xxx x xxxx x xx
+          xxx xx xx xxxx xxxx xxxx xxxxx xxxxxx xxxxx xxxxxx xxxx xxx xxx xx xxx
+          xx xxx xxx xxx xxx xxx xxx xxx xxxx xxx xx xxxx xxxx
+        </p>
+        <i class="el-icon-s-management" ref="top5">导航5</i>
+        <p>
+          xxx xxxxxx xxxxxxx xxxxxxx xxxx xxxx xxxx xx xxx xxxx xxxxx xxx xx
+          xxxxx xxx xxx xxxx xxx xxxx xxx xxxx xxxxx xxx xxx xxxxx xx xxx xxx
+          xxxx xxx xx xx xxx xx xx x xxx xx xxx xxx xxx xx xxx xxx xxx xxxx xxx
+          xxx xxx xx xx xxx xxx xx xxx xxxx xx xxx xxx xx xxx xxx x xxxx x xx
+          xxx xx xx xxxx xxxx xxxx xxxxx xxxxxx xxxxx xxxxxx xxxx xxx xxx xx xxx
+          xx xxx xxx xxx xxx xxx xxx xxx xxxx xxx xx xxxx xxxx
+        </p>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    goTop(id) {
+      let top = "";
+      if (id == 1) {
+        top = "top1";
+      } else if (id == 2) {
+        top = "top2";
+      } else if (id == 3) {
+        top = "top3";
+      } else if (id == 4) {
+        top = "top4";
+      }
 
-}
+      this.$refs[top].scrollIntoView({
+        behavior: "smooth", // 平滑过渡
+        block: "center", // start 上边框  center 中间  end 底部边框 与视窗顶部平齐
+      });
+    },
+  },
+};
 </script>
 
 <style>
+#content {
+  position: absolute;
+  /* height: 600px; */
+  width: 300px;
+  z-index: 999;
+}
+#doc i {
+  font-weight: bold;
+  font-size: 30px;
+}
 
 </style>
